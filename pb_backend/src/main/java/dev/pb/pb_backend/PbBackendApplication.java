e0495c17f130f076b8b1c203dc9d23600ee8f518
Willy <leaguematter@likelion.org>
2022-09-23 16:26:28 +0900

    settings

---
+++ pb_backend/src/main/java/dev/pb/pb_backend/PbBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.pb.pb_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbBackendApplication.class, args);
+	}
+
+}
---
+++ pb_backend/src/test/java/dev/pb/pb_backend/PbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pb.pb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
