4cfe547066b98b1533b632c5741c3f95edb6b12d
Will.Lee <leaguematter@likelion.org>
2022-10-07 11:23:16 +0900

    final modification before Presentation

--- pb_frontend/components/agri-map/AgriSpot.jsx
+++ pb_frontend/components/agri-map/AgriSpot.jsx
@@ -42,23 +42,17 @@ const AgriSpot = (props) => {
 
   return (
     <>
-      <svg viewBox={properties[curLocation]['viewBox']} xmlns="http://www.w3.org/2000/svg" width="calc(54% - 4rem)"  className={`${styles.position} ${styles[`position-${curLocation}`]}`}>
+      <svg viewBox={properties[curLocation]['viewBox']} xmlns="http://www.w3.org/2000/svg" width="80%"  className={`${styles.position} ${styles[`position-${curLocation}`]}`}>
         <g fill="white" stroke="green" stroke-width="5" transform={properties[curLocation]['transform']} onClick={modalChange}>
           {curCitiesOnProduct? list.map(item => {
             return curLocation === 'jeonbuk' ? 
-            <>
-              <image href={productImage} x={item[0]} y={item[1]} width='10rem' height='10rem' onClick={modalChange}/>
-              {modalVisible && <AgriListBlackout setIsModal={modalChange}/>}
-              {modalVisible && <AgriListDetail object={modalProduct}/>}
-            </> :
-            <>
-              <image href={productImage} x={item[0]} y={item[1]} width='2.5rem' height='2.5rem' onClick={modalChange}/>
-              {modalVisible && <AgriListBlackout setIsModal={modalChange}/>}
-              {modalVisible && <AgriListDetail object={modalProduct}/>}
-            </>
+              <image href={productImage} x={item[0]} y={item[1]} width='10rem' height='10rem' onClick={modalChange} className={styles['image']}/>:
+              <image href={productImage} x={item[0]} y={item[1]} width='2.5rem' height='2.5rem' onClick={modalChange} className={styles['image']}/>
           }) : <></>}
         </g>
       </svg>
+          {modalVisible && <AgriListBlackout setIsModal={modalChange}/>}
+          {modalVisible && <AgriListDetail object={modalProduct}/>}
     </>
   )
 }
