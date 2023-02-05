0976a1ef19a64344abf04a6b0a1448fb275f9f40
Will.Lee <leaguematter@likelion.org>
2023-02-05 21:18:24 +0900

    added dailyCrawler.py schedule function

--- dailyCrawler.py
+++ dailyCrawler.py
@@ -3,6 +3,7 @@ import pandas as pd
 import json
 from datetime import datetime
 from fake_useragent import UserAgent
+import schedule
 
 today = datetime.now().strftime('%Y-%m-%d')
 p_cert_key = '39ad110d-8304-44da-b584-38ec75b1f6e1'
@@ -21,56 +22,57 @@ location['COUNTRY_CODE'] = location['COUNTRY_CODE'].fillna(-1).astype('int16')
 location['LOCATION_ID'] = location['LOCATION_ID'].astype('int32')
 markets = location[location['COUNTRY_CODE'] != -1].loc[:, ['LOCATION_ID', 'CITY_NAME', 'COUNTRY_CODE']]
 
-userAgent = UserAgent()
-sucess = False
-market_idx = 0
-item_idx = 0
+def priceCrawling():
+  userAgent = UserAgent()
+  sucess = False
+  market_idx = 0
+  item_idx = 0
 
-while not sucess:
-  for i in range(market_idx, len(markets)):
-    locationId = markets.iloc[i, 0]
-    city = markets.iloc[i, 1][:2]
-    countryCode = markets.iloc[i, 2]
-    for j in range(item_idx, len(items)):
-      data = {}
-      p_itemcategorycode = items.iloc[j, 0]
-      p_itemcode = items.iloc[j, 1]
+  while not sucess:
+    for i in range(market_idx, len(markets)):
+      locationId = markets.iloc[i, 0]
+      city = markets.iloc[i, 1][:2]
+      countryCode = markets.iloc[i, 2]
+      for j in range(item_idx, len(items)):
+        data = {}
+        p_itemcategorycode = items.iloc[j, 0]
+        p_itemcode = items.iloc[j, 1]
 
-      try:
-        headers = {'User-Agent': userAgent.random}
-        url = f'http://www.kamis.or.kr/service/price/xml.do?action=periodProductList&p_productclscode=01&p_startday={p_startday}&p_endday={p_endday}&p_itemcategorycode={p_itemcategorycode}&p_itemcode={p_itemcode}&p_productrankcode={p_productrankcode}&p_countrycode={countryCode}&p_cert_key={p_cert_key}&p_cert_id={p_cert_id}&p_returntype=json'
-        req = requests.get(url, headers=headers)
-        print(f'{city}: {p_itemcode}')
+        try:
+          headers = {'User-Agent': userAgent.random}
+          url = f'http://www.kamis.or.kr/service/price/xml.do?action=periodProductList&p_productclscode=01&p_startday={p_startday}&p_endday={p_endday}&p_itemcategorycode={p_itemcategorycode}&p_itemcode={p_itemcode}&p_productrankcode={p_productrankcode}&p_countrycode={countryCode}&p_cert_key={p_cert_key}&p_cert_id={p_cert_id}&p_returntype=json'
+          req = requests.get(url, headers=headers)
+          print(f'{city}: {p_itemcode}')
+          
+          if req.content == b'':
+            continue
+
+          res = req.json()
+          
+          if type(res['data']) == list:
+            continue
+
+          df = pd.DataFrame(res['data']['item'])
+          df = df[df.price != '-']
+          price = df[df['countyname'].str.contains(city)].iloc[0, 6]
+          
+          if ',' in price:
+            priceValue = price.replace(',', '')
+          else:
+            priceValue = price
         
-        if req.content == b'':
+          data['locationId'] = str(locationId)
+          data['itemCode'] = str(p_itemcode)
+          data['priceDate'] = today
+          data['priceValue'] = int(priceValue)
+          
+          headers = {'content-type': 'application/json;charset=utf-8'}
+          requests.post('http://localhost:8090/prices', data=json.dumps(data), headers=headers)
+        
+        except:
+          market_idx, item_idx = i, j
           continue
 
-        res = req.json()
-        
-        if type(res['data']) == list:
-          continue
+    sucess = True
 
-        df = pd.DataFrame(res['data']['item'])
-        df = df[df.price != '-']
-        price = df[df['countyname'].str.contains(city)].iloc[0, 6]
-        
-        if ',' in price:
-          priceValue = price.replace(',', '')
-        else:
-          priceValue = price
-       
-        data['locationId'] = str(locationId)
-        data['itemCode'] = str(p_itemcode)
-        data['priceDate'] = today
-        data['priceValue'] = int(priceValue)
-        
-        headers = {'content-type': 'application/json;charset=utf-8'}
-        requests.post('http://localhost:8090/prices', data=json.dumps(data), headers=headers)
-      
-      except:
-        market_idx, item_idx = i, j
-        continue
-
-  sucess = True
-
-
+schedule.every().at("09:00").do(priceCrawling())
